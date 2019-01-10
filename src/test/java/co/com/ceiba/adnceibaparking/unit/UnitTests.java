2ab9c61a35206848a4f7d74352948807a1295fad
Mateo Gutiérrez Díaz - Ceiba Software <mateo.gutierrez@MacBook-Pro-de-Soporte-2.local>
2019-01-10 17:33:09 -0500

    Adding unit test

--- src/test/java/co/com/ceiba/adnceibaparking/unit/UnitTests.java
+++ src/test/java/co/com/ceiba/adnceibaparking/unit/UnitTests.java
@@ -5,6 +5,9 @@ import static org.mockito.Mockito.when;
 
 import co.com.ceiba.adnceibaparking.Models.TypeVehicle;
 import co.com.ceiba.adnceibaparking.exceptions.GeneralException;
+import co.com.ceiba.adnceibaparking.exceptions.NumberMaxVehicles;
+import co.com.ceiba.adnceibaparking.exceptions.PlateForDay;
+import co.com.ceiba.adnceibaparking.exceptions.VehicleRegisteredPreviously;
 import co.com.ceiba.adnceibaparking.utilities.DateConverter;
 import co.com.ceiba.adnceibaparking.utilities.Utils;
 import org.junit.Rule;
@@ -54,6 +57,79 @@ public class UnitTests {
     // TEST CASES
 
     @Test
+    public void testShouldListVehicles() {
+        // Arrange
+        List<Vehicle> vehicles = new ArrayList<Vehicle>();
+        getVehicles(vehicles);
+
+        // Act
+        when(vehicleRepository.findAll()).thenReturn(vehicles);
+        Response<List<Vehicle>> response = vehicleService.getAllVehicles();
+
+        // Assert
+        assertFalse(response.getData().isEmpty());
+    }
+
+    @Test
+    public void testShouldReturnMessageWhenParkingInEmpty() {
+        // Arrange
+        List<Vehicle> vehicles = new ArrayList<Vehicle>();
+
+        // Act
+        when(vehicleRepository.findAll()).thenReturn(vehicles);
+        Response<List<Vehicle>> response = vehicleService.getAllVehicles();
+
+        // Assert
+        assertEquals(response.getMessage(), Constants.NO_VEHICLES_IN_PARKING);
+    }
+
+    @Test
+    public void testShouldListTypeVehicles() {
+        // Arrange
+        List<TypeVehicle> typeVehicles = new ArrayList<TypeVehicle>();
+        getTypeVehicles(typeVehicles);
+
+        // Act
+        when(typeVehicleRepository.findAll()).thenReturn(typeVehicles);
+        Response<List<TypeVehicle>> response = typeVehicleService.getAllVehicleTypes();
+
+        // Assert
+        assertFalse(response.getData().isEmpty());
+    }
+
+    @Test
+    public void testShouldReturnMessageWhenDontThereAreVehicleTypes() {
+        // Arrange
+        List<TypeVehicle> typeVehicles = new ArrayList<TypeVehicle>();
+
+        // Act
+        when(typeVehicleRepository.findAll()).thenReturn(typeVehicles);
+        Response<List<TypeVehicle>> response = typeVehicleService.getAllVehicleTypes();
+
+        // Assert
+        assertEquals(response.getMessage(), Constants.NO_VEHICLE_TYPES_REGISTERED);
+    }
+
+    @Test
+    public void testShouldCalculateOneHourBetweenDateInAndDateOut() throws Exception{
+        // Arrange
+        double hourExpected = 1;
+        String dateIn = "08/01/2019 08:03:38";
+        String dateOut = "08/01/2019 09:03:38";
+        Date date1 = DateConverter.convertStringToDate(dateIn);
+        Date date2 = DateConverter.convertStringToDate(dateOut);
+        long hoursIn = date1.getTime();
+        long hoursOut = date2.getTime();
+
+        // Act
+        double hours = vehicleService.calculateHoursInParking(hoursOut, hoursIn);
+        hours = Math.ceil(hours);
+
+        // Assert
+        assertEquals(hours, hourExpected, 1.0);
+    }
+
+    @Test
     public void testShouldAddExtraPaymentToHighCylinderVehicle() {
         // Arrange
         Vehicle vehicle = new Vehicle(
@@ -83,53 +159,6 @@ public class UnitTests {
 
         // Assert
         assertEquals(response,false);
-    }
-
-    @Test
-    public void testShouldListVehicles() {
-        // Arrange
-        List<Vehicle> vehicles = new ArrayList<Vehicle>();
-        getVehicles(vehicles);
-
-        // Act
-        when(vehicleRepository.findAll()).thenReturn(vehicles);
-        Response<List<Vehicle>> response = vehicleService.getAllVehicles();
-
-        // Assert
-        assertFalse(response.getData().isEmpty());
-    }
-
-    @Test
-    public void testShouldCalculateOneHourBetweenDateInAndDateOut() throws Exception{
-        // Arrange
-        double hourExpected = 1;
-        String dateIn = "08/01/2019 08:03:38";
-        String dateOut = "08/01/2019 09:03:38";
-        Date date1 = DateConverter.convertStringToDate(dateIn);
-        Date date2 = DateConverter.convertStringToDate(dateOut);
-        long hoursIn = date1.getTime();
-        long hoursOut = date2.getTime();
-
-        // Act
-        double hours = vehicleService.calculateHoursInParking(hoursOut, hoursIn);
-        hours = Math.ceil(hours);
-
-        // Assert
-        assertEquals(hours, hourExpected, 1.0);
-    }
-
-    @Test
-    public void testShouldListTypeVehicles() {
-        // Arrange
-        List<TypeVehicle> typeVehicles = new ArrayList<TypeVehicle>();
-        getTypeVehicles(typeVehicles);
-
-        // Act
-        when(typeVehicleRepository.findAll()).thenReturn(typeVehicles);
-        Response<List<TypeVehicle>> response = typeVehicleService.getAllVehicleTypes();
-
-        // Assert
-        assertFalse(response.getData().isEmpty());
     }
 
     @Test
@@ -273,28 +302,82 @@ public class UnitTests {
         assertEquals(message, expected);
     }
 
-    @Test
+    @Test(expected = PlateForDay.class)
     public void testInsertVehicleFailureByPlateRule() throws GeneralException {
         // Arrange
-        String expected = Constants.VEHICLE_CANNOT_ENTER;
         Vehicle vehicle = new Vehicle(2,"ABC123","Eduardo López",750,"08/01/2019 08:03:38","Moto");
 
         // Act
-        try {
-            when(vehicleRepository.insert(vehicle)).thenReturn(vehicle);
-            Response<List<Vehicle>> response = vehicleService.registerVehicle(vehicle);
-
-        } catch (GeneralException e) {
-            // Assert
-            assertEquals(e.getMessage(), expected);
-        }
+        vehicleService.registerVehicle(vehicle);
     }
 
+    @Test(expected = NumberMaxVehicles.class)
+    public void testInsertVehicleFailureByMaxCapacity() throws GeneralException {
+        // Arrange
+        Vehicle vehicle = new Vehicle(2,"ABC123","Eduardo López",750,"08/01/2019 08:03:38","Moto");
+        List<Vehicle> vehicles = new ArrayList<Vehicle>();
+        getListVehiclesWithFullCapacity(vehicles);
+
+        // Act
+        when(vehicleRepository.findAllByTypeVehicleDescription(vehicle.getTypeVehicleDescription())).thenReturn(vehicles);
+        vehicleService.registerVehicle(vehicle);
+    }
+
+    @Test(expected = VehicleRegisteredPreviously.class)
+    public void testInsertVehicleAlreadyInParking() throws GeneralException  {
+        // Arrange
+        Vehicle vehicle = new Vehicle(2,"EXT333","Eduardo López",750,"08/01/2019 08:03:38","Moto");
+
+        // Act
+        when(vehicleRepository.findByPlate("EXT333")).thenReturn(vehicle);
+        vehicleService.registerVehicle(vehicle);
+    }
+
+    @Test
+    public void testShouldReturnTrueWhenVehicleIsAlreadyInRegistered() {
+        // Arrange
+        Vehicle vehicle = new Vehicle(2,"EXT333","Eduardo López",750,"08/01/2019 08:03:38","Moto");
+
+        // Act
+        when(vehicleRepository.findByPlate(vehicle.getPlate())).thenReturn(vehicle);
+        boolean isIn = vehicleService.carIsAlreadyRegistered(vehicle.getPlate());
+
+        // Assert
+        assertTrue(isIn);
+    }
+
+    @Test
+    public void testShouldReturnFalseWhenVehicleIsNotRegistered() {
+        // Arrange
+        Vehicle vehicle = new Vehicle(2,"EXT333","Eduardo López",750,"08/01/2019 08:03:38","Moto");
+
+        // Act
+        when(vehicleRepository.findByPlate(vehicle.getPlate())).thenReturn(null);
+        boolean isIn = vehicleService.carIsAlreadyRegistered(vehicle.getPlate());
+
+        // Assert
+        assertFalse(isIn);
+    }
+    
 
     static Response<List<Vehicle>> getVehicles(List<Vehicle> vehicleList) {
         vehicleList.add(new Vehicle(1,"EXT567","Eduardo López",0,"09/01/2019 18:03:38","Carro"));
         vehicleList.add(new Vehicle(2,"III999","Pepe Cabral",750,"08/01/2019 08:03:38","Moto"));
         vehicleList.add(new Vehicle(2,"QQQ111","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        return new Response<List<Vehicle>>(Constants.SUCCESS, vehicleList);
+    }
+
+    static Response<List<Vehicle>> getListVehiclesWithFullCapacity(List<Vehicle> vehicleList) {
+        vehicleList.add(new Vehicle(2,"1","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"2","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"3","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"4","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"5","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"6","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"7","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"8","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"9","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
+        vehicleList.add(new Vehicle(2,"10","Ramiro Castrillón",100,"09/01/2019 15:03:38","Moto"));
         return new Response<List<Vehicle>>(Constants.SUCCESS, vehicleList);
     }
 
